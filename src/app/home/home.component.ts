26e75e5c80ca85cd6de8bde06f6a47427cd53ba7
Rithysak Sourng <Sourngrithysak@gmail.com>
2025-03-28 20:11:05 +0700

    angular commit part 2

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  /*
+    * title label
+  */
+  title: string = "Your Information"
+  /*
+    * information
+  */
+  id: number = 1
+  name: string = "Sourng Rithysak"
+  age: number = 20
+  email: string = "Sourngrithysak@gmail.com"
+  mobile: string = "+855 96 9768 713"
+}
